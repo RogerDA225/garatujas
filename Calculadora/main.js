67d13b817ee8fc1b46a3e2076cb6f9c90943c6f8
RogerDA225 <roger.albineli@estudantes.ifc.edu.br>
2026-09-01 11:41:56 -0300

    Add files via upload
    Calculadora

---
+++ Calculadora/main.js
@@ -0,0 +1,9 @@
+const inp = document.querySelector(".calc input")
+const btns = document.querySelectorAll(".calc .num")
+
+
+btns.forEach(function(bt) {
+    bt.onclick = function() {
+        inp.value += bt.textContent
+    }
+})
